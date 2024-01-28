26220bd161ed9d003018463ee61446b469ad4de0
KaterinaMysl <myslivchik382@gmail.com>
2024-01-28 16:19:07 +0300

    feat: add read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,15 @@
+import { dirname } from 'path';
+import fs from "fs/promises";
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    try {
+        const readFile = await fs.readFile(`${__dirname}/files/fileToRead.txt`, 'utf-8');
+        console.log(readFile);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
