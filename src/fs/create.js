7a9216fa4d6b35f59d02a40a4da5e76324400398
KaterinaMysl <myslivchik382@gmail.com>
2024-01-28 14:26:49 +0300

    feat: add create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+import { dirname } from 'path';
+import fs from "fs/promises";
+import { fileURLToPath } from 'url';
+
 const create = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+
+    try {
+        await fs.writeFile(`${__dirname}/files/fresh.txt`, "I am fresh and young", { flag: 'wx' });
+    } catch (error) {
+        throw new Error("FS operation failed");
+    }
 };
 
 await create();
