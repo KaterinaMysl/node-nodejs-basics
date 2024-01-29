8ebf072e6dc4271a93f19ccc07437e2af53f2735
KaterinaMysl <myslivchik382@gmail.com>
2024-01-29 19:15:12 +0300

    feat: add esm

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,16 +1,23 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path, { dirname } from "path";
+import { release, version } from "os";
+import { createServer as createServerHttp } from "http";
+import "./files/c.js";
+
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = JSON.parse(readFileSync(`${__dirname}/files/a.json`));
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = JSON.parse(readFileSync(`${__dirname}/files/a.json`));
 }
 
 console.log(`Release ${release()}`);
@@ -33,7 +40,7 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export {
     unknownObject,
     myServer,
 };
